b80809f16bd944bd3298763e78ae294354cbdadb
Anthony BRENELIERE <a.breneliere@imaprotect.fr>
2020-04-29 15:56:32 +0200

    En http lt notification push service émettait des erreurs

--- src/app/services/push-notification.service.ts
+++ src/app/services/push-notification.service.ts
@@ -23,74 +23,93 @@ export class PushNotificationService {
   /** Cette fonction vérifie la compatibilité du navigateur de l'utilisateur aux service worker et  notifications push
     * Si Le navigateur est compatible, il inscrit le serviceWorker
      */
-  init() {
+  init()
+  {
     console.log("Initialisation du service worker");
-    if ('serviceWorker' in navigator && 'PushManager' in window) {
+    if ('serviceWorker' in navigator )
+    {
+      if ( 'PushManager' in window )
+      {
+        console.log("navigateur compatible service worker et push");
 
-      console.log("navigateur compatible service worker et push");
-      navigator.serviceWorker.register('assets/sw.js')
-        .then(swReg => {
-          console.log('Service Worker is registered', swReg);
+        navigator.serviceWorker.register('assets/sw.js')
+          .then(swReg => {
+            console.log('Service Worker is registered', swReg);
+            this.swRegistration = swReg;
+            //this.checkSubscription();
+          })
+          .catch(error => {
+            console.error('Service Worker Error', error);
+          });
 
-          this.swRegistration = swReg;
-          //this.checkSubscription();
-        })
-        .catch(error => {
-          console.error('Service Worker Error', error);
-        });
-      this.pushNotificationStatus.isSupported = true;
-    } else {
-      console.log("navigateur pas compatible service worker et push");
-      this.pushNotificationStatus.isSupported = false;
+        this.pushNotificationStatus.isSupported = true;
+      }
+      else
+      {
+        console.warn("navigateur pas compatible service worker et push");
+        this.pushNotificationStatus.isSupported = false;
+      }
+
+      navigator.serviceWorker.addEventListener('message', (event) =>
+      {
+        this.notifications.push(event.data);
+      });
+
     }
 
-    navigator.serviceWorker.addEventListener('message', (event) => {
-      this.notifications.push(event.data);
-    });
+
 
   }
 
-  /** Cette fonction créé une souscription, si l'utilisateur n'a pas deja une souscription, dans le pushManager du service worker
+  /**
+   * Cette fonction créé une souscription, si l'utilisateur n'a pas deja une souscription, dans le pushManager du service worker
    * et envoie les infos de la souscription au serveur ImaInter
    */
-  subscribeUser() {
+  subscribeUser()
+  {
+    if ( ! this.swRegistration )
+    {
+      console.warn("Pas de service de souscription de notification dans le navigateur.");
+      return;
+    }
+
     const applicationServerKey = this.urlB64ToUint8Array(environment.applicationServerPublicKey);
 
     // On récupère la souscription existante dans le pushManager
-    this.swRegistration.pushManager.getSubscription()
-      .then(
-        subscription => {
-          if (subscription == null) {
-            // Si l'utilisateur n'a pas de souscription, on lui créé une souscription
-            this.swRegistration.pushManager.subscribe({
-              userVisibleOnly: true,
-              applicationServerKey: applicationServerKey
-            })
-              .then(subscription => {
-                console.log(`utilisateur "${this._connectionStatus.login}"`)
+    this.swRegistration.pushManager.getSubscription().then( subscription =>
+    {
+      if (subscription == null)
+      {
+        // Si l'utilisateur n'a pas de souscription, on lui créé une souscription
+        this.swRegistration.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: applicationServerKey
+        })
+        .then(subscription => {
+          console.log(`utilisateur "${this._connectionStatus.login}"`)
 
-                // On envoie les informations de la souscription utilisateur à ImaInterService
-                this.addOrUpdateUserSubscription(this._connectionStatus.login, subscription);
-                this.pushNotificationStatus.isSubscribed = true;
-                console.log('une nouvelle souscription a été créé');
-              })
-              .catch(err => {
-                console.error('Erreur à la création de la souscription: ', err);
-                // console.log('DOMEXCEPTION MESSAGE: ', err.message);
-                // console.log('DOMEXCEPTION NAME: ', err.name);
-                // console.log('DOMEXCEPTION CODE: ', err.code);
-                return null;
-              });
-          }
-          // Si la souscription utilisateur existe deja,
-          // On envoie toute de meme les informations au cas ou celle ci n'aurait pas été envoyé au serveur
-          else {
-            this.addOrUpdateUserSubscription(this._connectionStatus.login, subscription);
-            console.log('la souscription existante a été récupéré');
-          }
-          console.log(subscription);
-        }
-      )
+          // On envoie les informations de la souscription utilisateur à ImaInterService
+          this.addOrUpdateUserSubscription(this._connectionStatus.login, subscription);
+          this.pushNotificationStatus.isSubscribed = true;
+          console.log('une nouvelle souscription a été créé');
+        })
+        .catch(err => {
+          console.error('Erreur à la création de la souscription: ', err);
+          // console.log('DOMEXCEPTION MESSAGE: ', err.message);
+          // console.log('DOMEXCEPTION NAME: ', err.name);
+          // console.log('DOMEXCEPTION CODE: ', err.code);
+          return null;
+        });
+      }
+      // Si la souscription utilisateur existe deja,
+      // On envoie toute de meme les informations au cas ou celle ci n'aurait pas été envoyé au serveur
+      else {
+        this.addOrUpdateUserSubscription(this._connectionStatus.login, subscription);
+        console.log('la souscription existante a été récupéré');
+      }
+
+      console.log(subscription);
+    });
   }
 
   /** Cette fonction appelle le hub pour ajouter ou mettre à jour une souscription utilisateur
