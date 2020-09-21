ed5fb7865468a052e276422a6d9d3239b7b66f70
Gaétan MACE <g.mace@imaprotect.fr>
2020-09-21 12:14:02 +0200

    ngModelChangeDebounced : add example

--- src/app/ngModelChangeDebouncedDirective.ts
+++ src/app/ngModelChangeDebouncedDirective.ts
@@ -3,6 +3,19 @@ import {debounceTime, distinctUntilChanged, map, skip} from 'rxjs/operators';
 import {Directive, EventEmitter, Input, OnDestroy, Output} from '@angular/core';
 import {Subscription} from 'rxjs/internal/Subscription';
 
+//(ngModelChange)="searchInterventions()" >
+
+/**
+ * Cette directive permet d'attendre 500 millisecondes avant de valider les modifications apportés
+ * Cela permet notamment de temporiser dans le cas du lancement d'une action après une modification utilisateur 
+ * et ainsi éviter de lancer plusieurs fois une action à tord (ex: le déclenchement d'une recherche alors que l'utlisateur est encore en train d'écrire)
+ * 
+ * Cette directive s'utilise de la meme manière que ngModelChange : 
+ * on peut donc remplacer ça : 
+ * <input #searchInput [(ngModel)]="Search.FreeQuery" (ngModelChange)="searchInterventions()" />
+ * par cela ce qui suit,si l'on souhaite attendre 500 ms avant d'appeler la recherche d'interventions :
+ * <input #searchInput [(ngModel)]="Search.FreeQuery" (ngModelChangeDebounced)="searchInterventions()" />
+ */
 @Directive({
   selector: '[ngModelChangeDebounced]',
 })
